f8a1fc60bb44d6ee27caa390157b52a245bf9964
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-10-30 21:43:31 +0100

    init project

---
+++ src/routes/lists.routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import db from '../db';
+// **Hier könntest du dein List Modell importieren, wenn es existiert**
+
+const router = express.Router();
+
+// Route, um alle Einkaufslisten abzurufen
+router.get('/', async (req, res) => {
+    // Hier würde später der Code stehen, um alle Listen abzurufen
+    res.send('Hier könnten alle Einkaufslisten angezeigt werden.');
+});
+
+// Route, um eine neue Einkaufsliste zu erstellen
+router.post('/', async (req, res) => {
+    // Hier würde später der Code stehen, um eine neue Liste zu erstellen
+    res.send('Hier könnte eine neue Einkaufsliste erstellt werden.');
+});
+
+export default router;
