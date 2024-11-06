38d11d9e05d175e120989ca48ceafd1f326254eb
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-11-06 15:50:27 +0100

    fix: delete unneccesary migrationsfolder

--- backend/src/config/drizzle.config.ts
+++ backend/src/config/drizzle.config.ts
@@ -4,7 +4,7 @@ import { ENV } from './env.config';
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema',
-  out: '../db/migrations',
+  out: './src/db/migrations',
   casing: 'snake_case',
   dbCredentials: {
     url: ENV.DATABASE_URL,
