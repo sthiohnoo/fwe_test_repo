69bfdfcf71141af5efd2e49b2f79aa4677bdb969
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-11-08 19:13:20 +0100

    add zod error and tokenexpirederror in global-error

--- backend/src/utils/global-error.ts
+++ backend/src/utils/global-error.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 export function globalErrorHandler(
   err: Error,
@@ -6,6 +8,20 @@ export function globalErrorHandler(
   res: Response,
   _next: NextFunction,
 ) {
+  if (err instanceof ZodError) {
+    res.status(400).json({
+      errors: err.errors,
+    });
+    return;
+  }
+
+  if (err instanceof TokenExpiredError) {
+    res.status(401).json({
+      errors: ['Token expired'],
+    });
+    return;
+  }
+
   console.error('Unhandled error:', err);
 
   // Default error response
