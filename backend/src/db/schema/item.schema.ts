52f8e376432396f518b379da4d7a61188e1a04f8
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-11-09 13:23:48 +0100

    feat: add relations to all schema

--- backend/src/db/schema/item.schema.ts
+++ backend/src/db/schema/item.schema.ts
@@ -1,6 +1,12 @@
+import { relations } from 'drizzle-orm';
 import { pgTable } from 'drizzle-orm/pg-core';
 import { commonSchema } from './common.schema';
+import { shoppingListItem } from './shoppingListItem.schema';
 
 export const item = pgTable('item', {
   ...commonSchema,
 });
+
+export const itemRelations = relations(item, ({ many }) => ({
+  shoppingListItems: many(shoppingListItem),
+}));
--- backend/src/db/schema/shoppingList.schema.ts
+++ backend/src/db/schema/shoppingList.schema.ts
@@ -1,8 +1,14 @@
+import { relations } from 'drizzle-orm';
 import { pgTable, timestamp } from 'drizzle-orm/pg-core';
 
 import { commonSchema } from './common.schema';
+import { shoppingListItem } from './shoppingListItem.schema';
 
 export const shoppingList = pgTable('shopping_list', {
   ...commonSchema,
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+
+export const shoppingListRelations = relations(shoppingList, ({ many }) => ({
+  shoppingListItems: many(shoppingListItem),
+}));
