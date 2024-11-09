a3b25b9275054bbf776865c4b6da3137f5cb88a5
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-11-09 11:47:40 +0100

    refactor rename tables & migrate

--- backend/src/db/schema/shoppingList.schema.ts
+++ backend/src/db/schema/shoppingList.schema.ts
@@ -8,16 +8,16 @@ import {
 
 import { commonSchema } from './common.schema';
 
-export const shoppingList = pgTable('shopping_lists', {
+export const shoppingList = pgTable('shopping_list', {
   ...commonSchema,
   createdAt: timestamp('created_at').defaultNow().notNull(),
 });
 
-export const item = pgTable('items', {
+export const item = pgTable('item', {
   ...commonSchema,
 });
 
-export const shoppingListItem = pgTable('shopping_list_items', {
+export const shoppingListItem = pgTable('shopping_list_item', {
   listId: uuid('list_id')
     .references(() => shoppingList.id)
     .notNull(),
